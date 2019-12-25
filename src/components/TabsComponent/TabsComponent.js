c8070771d38deb872ea6c7f1c027c7259dbaff2f
Jin Ying <kenneth.jyt@gmail.com>
2019-12-25 23:35:41 +0800

    TabsComponent.js: Format Indentations

--- src/components/TabsComponent/TabsComponent.js
+++ src/components/TabsComponent/TabsComponent.js
@@ -12,44 +12,44 @@ import VariableRiskContainerComponent from './VariableRiskContainerComponent';
 import FixedRiskContainerCompnent from './FixedRiskContainerComponent';
 
 function TabsComponent() {
-    const [value, setValue] = React.useState(0);
-    const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  const theme = useTheme();
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    
-    const handleChangeIndex = index => {
-        setValue(index);
-    };
-    
-    return (
-        <React.Fragment>
-            <CssBaseline />
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-            <Container maxWidth="lg" disableGutters>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    variant="fullWidth"
-                    aria-label="Inputs"
-                >
-                    <Tab label="Variable Risk" id="variable-risk" aria-controls="variable-risk-panel" />
-                    <Tab label="Fixed Risk" id="fixed-risk-panel" aria-controls="fixed-risk-panel" />
-                </Tabs>
-                <SwipeableViews
-                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                    index={value}
-                    onChangeIndex={handleChangeIndex}
-                >
-                    <VariableRiskContainerComponent value={value} index={0} dir={theme.direction} />
-                    <FixedRiskContainerCompnent value={value} index={1} dir={theme.direction} />
-                </SwipeableViews>
-            </Container>
-        </React.Fragment>
-    )
+  const handleChangeIndex = index => {
+    setValue(index);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <Container maxWidth="lg" disableGutters>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="Inputs"
+        >
+          <Tab label="Variable Risk" id="variable-risk" aria-controls="variable-risk-panel" />
+          <Tab label="Fixed Risk" id="fixed-risk-panel" aria-controls="fixed-risk-panel" />
+        </Tabs>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <VariableRiskContainerComponent value={value} index={0} dir={theme.direction} />
+          <FixedRiskContainerCompnent value={value} index={1} dir={theme.direction} />
+        </SwipeableViews>
+      </Container>
+    </React.Fragment>
+  )
 }
 
 
