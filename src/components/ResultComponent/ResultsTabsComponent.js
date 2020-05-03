d2accd6742a73ffe0da5469ce5582e8435646a02
Jin Ying <kenneth.jyt@gmail.com>
2020-05-03 19:12:07 +0800

    Fix trades table not hidden error

--- src/components/ResultComponent/ResultsTabsComponent.js
+++ src/components/ResultComponent/ResultsTabsComponent.js
@@ -10,10 +10,28 @@ import Tab from '@material-ui/core/Tab';
 import MonthlyResultsComponent from './MonthlyResultsComponent/MonthlyResultsComponent'
 import TradesResultsComponent from './TradesResultsComponent/TradesResultsComponent'
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+          children
+      )}
+    </div>
+  );
+}
+
 function ResultsTabsComponent(props) {
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
-  const { tradeParameters, classes } = props;
+  const { tradeParameters } = props;
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -26,37 +44,34 @@ function ResultsTabsComponent(props) {
   return (
     <Box mb={5}>
       <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="Inputs"
-        >
-          <Tab label="Monthly Returns" id="monthly-returns" aria-controls="monthly-returns-panel" />
-          <Tab label="Table of Results" id="table-results" aria-controls="table-results-panel" />
-          <Tab label="Graph" id="graph" aria-controls="graph-panel" />
-        </Tabs>
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        variant="fullWidth"
+        aria-label="Inputs"
+      >
+        <Tab label="Monthly Returns" id="monthly-returns" aria-controls="monthly-returns-panel" />
+        <Tab label="Table of Results" id="table-results" aria-controls="table-results-panel" />
+        <Tab label="Graph" id="graph" aria-controls="graph-panel" />
+      </Tabs>
 
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-          <MonthlyResultsComponent 
-            value={value} 
-            index={0} 
-            dir={theme.direction}
-            portfolio={tradeParameters.portfolioResults.monthlyPortfolio} 
-          />
-          <TradesResultsComponent
-            value={value} 
-            index={1} 
-            dir={theme.direction}
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <MonthlyResultsComponent portfolio={tradeParameters.portfolioResults.monthlyPortfolio}/>
+        </TabPanel>
+
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <TradesResultsComponent 
             portfolio={tradeParameters.portfolioResults.portfolio}
             trades={tradeParameters.trades}
-          /> 
-        </SwipeableViews>
+          />
+        </TabPanel>
+      </SwipeableViews>
     </Box>
   )
 }
